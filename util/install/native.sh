8242152f10002be9c3016e3ea18c9ef8c2b9449f
angonz <andres@aulasneo.com>
2019-10-17 12:07:52 +0000

    Aulasneo: symlink ssl certificate instead of copy. Changed to use aulasneo repo

--- util/install/native.sh
+++ util/install/native.sh
@@ -147,9 +147,9 @@ CONFIGURATION_VERSION=${CONFIGURATION_VERSION-$OPENEDX_RELEASE}
 ## Clone the configuration repository and run Ansible
 ##
 cd /var/tmp
-git clone https://github.com/edx/configuration
+git clone https://github.com/aulasneo/configuration
 cd configuration
-git checkout $CONFIGURATION_VERSION
+git checkout aulasneo
 git pull
 
 ##
